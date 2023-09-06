bab642bee5432237866309420bc78f71eba56c68
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-09-06 16:11:36 +0200

    dashboard adjustments

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,8 +6,8 @@
 
     <div class="section px-5 border-top border-secondary pt-5" id="admin-dashboard">
         <div class="row px-5 mx-5 g-2">
-            <div class="col-6">
-                <div class="card rounded-bottom rounded-end rounded-5 text-center" id="card-index">
+            <div class="col-4">
+                <div class="card rounded-bottom rounded-start rounded-5 text-center" id="card-index">
                     <div class="card-head p-3">
                         <h5 class="card-title"></h5>
                     </div>
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-4">
                 <div class="card rounded-bottom rounded-start rounded-5 text-center" id="card-create">
                     <div class="card-head p-3">
                         <h5 class="card-title"></h5>
@@ -28,13 +28,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12">
-                <div class="card rounded-top rounded-5 text-center" id="card-counter">
+            <div class="col-4">
+                <div class="card rounded-bottom rounded-start rounded-5 text-center" id="card-counter">
                     <div class="card-head p-3">
                         <h5 class="card-title"></h5>
                     </div>
                     <div class="card-body d-flex align-items-center justify-content-center">
-                        <p>CURRENTLY STORED PROJECTS: <span class="fw-bold">{{ $projectCount }}</span></p>
+                        <p>CURRENTLY STORED:<br><span class="fw-bold display-2">{{ $projectCount }}</span></p>
                     </div>
                 </div>
             </div>
